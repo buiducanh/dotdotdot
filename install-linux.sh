6aa456191f8af61abd92bb301a6b14acc091e0d9
Anh Bui <b.ducanh96@gmail.com>
2021-01-04 23:46:44 -0800

    use install_tmux.sh script in install-linux

--- install-linux.sh
+++ install-linux.sh
@@ -1,130 +1,115 @@
 #!/bin/bash
 
-if [[ $EUID != 0 ]]; then
-  sudo "$0" "$@"
-  exit $?
+runAsRoot() {
+	echo "${@:2}"
+  sudo "$0" "${@:2}"
+}
+
+isGitHasUsername() {
+	TMP=$(grep "name = Anh Bui" $HOME/.gitconfig)
+	if [[ -z "$TMP" ]]; then
+		return "0"
+	else
+		return "1"
+	fi
+}
+
+isGitHasEmail() {
+	TMP=$(grep "email = b.ducanh96@gmail.com" $HOME/.gitconfig)
+	if [[ -z "$TMP" ]]; then
+		return "0"
+	else
+		return "1"
+	fi
+}
+
+# parse options
+SHOULD_OVERRIDE_HOME=
+HOME_OVERRIDE=
+while getopts ":h:o" opt; do
+	case $opt in
+		h) HOME_OVERRIDE=$OPTARG
+			;;
+		o) SHOULD_OVERRIDE_HOME=1
+	esac
+done
+
+if [[ "$SHOULD_OVERRIDE_HOME" == 1 ]]; then
+	HOME="$HOME_OVERRIDE"
 fi
 
-install() {
-  if command -v apt-get >/dev/null 2>&1; then
-    sudo apt-get install -y "$@"
-  fi
-}
 
-symlinkSafe() {
-  target="$1"
-  dest="$2"
-  tmpdest="$dest"".orig"
-  if  [[ -e "$dest" ]]; then
-    if [[ ! -L "$dest" ]]; then
-      mv "$dest" "$tmpdest"
-    fi
-  fi
-  ln -sf "$target" "$dest"
-}
+if [[ $EUID != 0 ]]; then
+	runAsRoot "$0" "$@" -h "$HOME"
+  exit $?  else if [[ "$HOME" == "/" || "$HOME" == "/root" ]]; then echo
+  "Defaults env_keep += \"HOME\"" > /etc/sudoers.d/1_homeenv runAsRoot "$0"
+  "$@" -h "$HOME_OVERRIDE" -o exit 1 fi fi
 
-clone() {
-  username="$1"
-  target="$2"
-  dest="$3"
-  su "$username" -c "git clone $target ${dest//\~/$HOME}"
-}
+install() { if command -v apt-get >/dev/null 2>&1; then echo "install $@" sudo
+	apt-get install -y "$@" else echo "no apt-get, did not install $@" fi }
 
-uninstall() {
-  if command -v apt-get >/dev/null 2>&1; then
-    sudo apt-get remove -y "$@"
-  fi
-}
+symlinkSafe() { target="$1" dest="$2" tmpdest="$dest"".orig" if  [[ -e "$dest"
+	]]; then if [[ ! -L "$dest" ]]; then mv "$dest" "$tmpdest" fi fi ln -sf
+	"$target" "$dest" }
+
+clone() { username="$1" target="$2" dest="$3" su "$username" -c "git clone
+	$target ${dest//\~/$HOME}" }
+
+uninstall() { if command -v apt-get >/dev/null 2>&1; then echo "uninstall $@"
+	sudo apt-get remove -y "$@" else echo "no apt-get, did not uninstall
+	$@" fi }
 
 # echo "Set up xsel"
 
-# if ! command -v xsel >/dev/null 2>&1; then
-#   install xsel
-# fi
-# if ! command -v xclip >/dev/null 2>&1; then
-#   uninstall xclip
-# fi
+# if ! command -v xsel >/dev/null 2>&1; then install xsel fi if ! command -v
+# xclip >/dev/null 2>&1; then uninstall xclip fi
 
-userlist=$(dirname "$HOME")
-username=$(ls "$userlist" -1 | head -n1)
+home=$(dirname "$HOME") echo "$home" userlist=$(dirname "$HOME") echo "User
+list: $userlist" username=$(ls "$userlist" -1 | head -n1) echo "Username:
+$username"
 
-sudo apt-get update -y
-if ! command -v git >/dev/null 2>&1; then
-  echo "Install git"
-  install git
-  name=$(read -p "input your name for Git: ")
-  email=$(read -p "input your email for Git: ")
-  /usr/bin/git config --global user.name "$name"
-  /usr/bin/git config --global user.email "$email"
-  if [[ ! -f ~/.ssh/id_rsa ]]; then
-    echo "Prepare git ssh key"
-    ssh-keygen -t rsa -b 4096 -c "$email"
-  fi
-fi
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+sudo apt-get update -y if ! command -v git >/dev/null 2>&1; then install git fi
 
-if ! command -v keychain >/dev/null 2>&1; then
-  echo "Install keychain"
-  install keychain
-fi
+echo ".gitconfig" cat ~/.gitconfig if [[ isGitHasUsername = "0" ]]; then
+name=$(read -p "input your name for Git: ") /usr/bin/git config --global
+user.name "$name" fi
 
-echo "Prepare projects directory"
-mkdir -p ~/projects
+if [[ isGitHasEmail = "0" ]]; then email=$(read -p "input your email for Git:
+	") /usr/bin/git config --global user.email "$email" fi
 
-echo "Prepare dotfiles"
-if [[ ! -d "$HOME/projects/dotdotdot" ]]; then
-  clone "$username" git@github.com:buiducanh/dotdotdot.git ~/projects/
-else
-  (cd ~/projects/dotdotdot && git pull)
-fi
+if [[ ! -f ~/.ssh/id_rsa ]]; then echo "Prepare git ssh key" ssh-keygen -t rsa
+	-b 4096 -c "$email" fi eval $(ssh-agent -s) ssh-add ~/.ssh/id_rsa
 
-echo "Prepare build toolchains"
-if ! command -v make >/dev/null 2>&1; then
-  install build-essential
-fi
+if ! command -v keychain >/dev/null 2>&1; then install keychain fi
 
-if ! command -v cmake >/dev/null 2>&1; then
-  install cmake
-fi
+echo "Prepare projects directory" mkdir -p ~/projects
 
-if ! command -v aclocal >/dev/null 2>&1; then
-  install autotools-dev
-  install automake
-fi
+echo "Prepare dotfiles" if [[ ! -d "$HOME/projects/dotdotdot" ]]; then clone
+"$username" git@github.com:buiducanh/dotdotdot.git ~/projects/ else (cd
+~/projects/dotdotdot && git pull) fi
 
-echo "Prepare .ssh config"
-symlinkSafe ~/projects/dotdotdot/.ssh/config ~/.ssh/config
+echo "Prepare build toolchains" if ! command -v make >/dev/null 2>&1; then
+install build-essential fi
 
-echo "Prepare git prompt"
-symlinkSafe ~/projects/dotdotdot/git-prompt.sh ~/.git-prompt.sh
+if ! command -v cmake >/dev/null 2>&1; then install cmake fi
 
-echo "Prepare bashrc"
-symlinkSafe ~/projects/dotdotdot/.bashrc ~/.bashrc
+if ! command -v aclocal >/dev/null 2>&1; then install autotools-dev install
+	automake fi
 
-echo "Prepare bash_aliases"
-symlinkSafe ~/projects/dotdotdot/.bash_aliases ~/.bash_aliases
+echo "Prepare .ssh config" symlinkSafe ~/projects/dotdotdot/.ssh/config
+~/.ssh/config
 
-echo "Prepare inputrc"
-symlinkSafe ~/projects/dotdotdot/.inputrc ~/.inputrc
+echo "Prepare git prompt" symlinkSafe ~/projects/dotdotdot/git-prompt.sh
+~/.git-prompt.sh
 
-if [[ ! $(echo "$(tmux -V | cut -d' ' -f2) > 2" | bc) -eq 1 ]]; then
-  echo "Install tmux"
-  if command -v tmux >/dev/null 2>&1; then
-    echo "Uninstall tmux version below 2"
-    uninstall tmux
-  fi
-  tmpdir=$(mktemp -d)
-  install libevent-dev
-  install libncurses5-dev
-  pushd "$tmpdir"
-  clone "$username" https://github.com/tmux/tmux.git
-  sh autogen.sh \
-  && ./configure && make
-  rm /usr/bin/tmux
-  mv ./tmux/tmux /usr/bin/
-  popd
-fi
+echo "Prepare bashrc" symlinkSafe ~/projects/dotdotdot/.bashrc ~/.bashrc
+
+echo "Prepare bash_aliases" symlinkSafe ~/projects/dotdotdot/.bash_aliases
+~/.bash_aliases
+
+echo "Prepare inputrc" symlinkSafe ~/projects/dotdotdot/.inputrc ~/.inputrc
+
+./install_tmux.sh
 
 echo "Prepare tmux.conf"
 symlinkSafe ~/projects/dotdotdot/.tmux.conf ~/.tmux.conf
